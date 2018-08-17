c94010cecacd97c0f09a7aab6300b780c160e961
peaceengine <antonybutts@me.com>
2018-08-17 14:01:12 +0100

    done jobs

--- portfolioProject/views.py
+++ portfolioProject/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import operator
 
-def homepage(request):
-    return render(request, 'home.html')
+#def homepage(request):
+#    return render(request, 'home.html')
