9c468cb4d05cd6a23165150f11854dff7b8577ee
peaceengine <antonybutts@me.com>
2018-08-20 13:08:25 +0100

    finished course. still loadsa stuff to do

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,16 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        wordList = self.body.split()
+        shortened = ""
+        for i in range(min(20, len(wordList))):
+            shortened += " " + wordList[i]
+        return (shortened + " ...")
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%b %e %Y")
